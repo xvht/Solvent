5290750287ec842ef20f071d7a49f722bcbdc84f
devfxx <54813298+devfxx@users.noreply.github.com>
2025-01-15 02:40:54 +0200

    feat: Add endpoints to retrieve user friends and their count

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,66 @@ app.get("/", (c) => {
   return c.json({ success: false, error: "No user ID provided" });
 });
 
+app.get("/:id/friends", async (c) => {
+  try {
+    const id = Number(c.req.param("id"));
+    if (!id || isNaN(id)) {
+      return c.json({
+        success: false,
+        error: "Invalid user ID",
+      });
+    }
+
+    const request = await fetch(
+      `https://friends.roblox.com/v1/users/${id}/friends?sortOrder=Asc&limit=100`
+    );
+    return c.json({ success: true, data: await request.json() });
+  } catch (error) {
+    c.status(500);
+    return c.json({
+      success: false,
+      error: "An error occurred while fetching the data.",
+    });
+  }
+});
+
+app.get("/:id/friends/count", async (c) => {
+  try {
+    const id = Number(c.req.param("id"));
+    if (!id || isNaN(id)) {
+      return c.json({
+        success: false,
+        error: "Invalid user ID",
+      });
+    }
+
+    let nextPageCursor = "";
+    let totalCount = 0;
+
+    do {
+      const cursorParam = nextPageCursor ? `&cursor=${nextPageCursor}` : "";
+      const request = await fetch(
+        `https://friends.roblox.com/v1/users/${id}/friends?sortOrder=Asc&limit=100${cursorParam}`
+      );
+      const data = await request.json();
+
+      totalCount += data.data.length;
+      nextPageCursor = data.nextPageCursor;
+    } while (nextPageCursor);
+
+    return c.json({
+      success: true,
+      totalFriends: totalCount,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json({
+      success: false,
+      error: "An error occurred while fetching the data.",
+    });
+  }
+});
+
 app.get("/:id/following", async (c) => {
   try {
     const id = Number(c.req.param("id"));
