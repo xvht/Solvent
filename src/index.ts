c4ddd05264f04705e3a94513f7f8488faa861c4e
xlarp <freedom@xvh.lol>
2025-03-08 17:23:58 +0200

    feat: Add CORS middleware to Hono application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { badRequestResponse } from "./responses";
 import router from "./routes/v1";
 
 const app = new Hono();
 
+app.use(cors());
 app.route("/v1", router);
 
 app.get("*", (c) => {
