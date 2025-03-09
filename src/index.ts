28f8a491659a81ce5ac918af2568a943e73a29ca
xlarp <freedom@xvh.lol>
2025-03-09 11:55:50 +0200

    feat: Add trailing slash trimming middleware to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { trimTrailingSlash } from "hono/trailing-slash";
 import { badRequestResponse } from "./responses";
 import router from "./routes/v1";
 
 const app = new Hono();
 
 app.use(cors());
+app.use(trimTrailingSlash());
 app.route("/v1", router);
 
 app.get("*", (c) => {
